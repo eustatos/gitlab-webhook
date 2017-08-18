f387439b10a0331ff14f9be8164acff4f608b8f6
eustatos <astashkinav@gmail.com>
2017-08-18 17:20:40 +0400

    style(Event): add space

--- Event.php
+++ Event.php
@@ -48,7 +48,7 @@ class Event
         echo 'Result execution: ' . $this->_command . PHP_EOL
         . $this->_result . PHP_EOL;
         echo '<pre>';
-            foreach($this->_output as $outputString) {
+            foreach ($this->_output as $outputString) {
                 echo $outputString . '<br>';
             }
         echo '</pre>';
